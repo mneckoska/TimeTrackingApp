02a877a66acc17e1a16f024d39bc3cad2cbcf7ef
mneckoska <mneckoska@yahoo.com>
2020-11-14 13:02:27 +0100

    refactoring

--- server/models/project.js
+++ server/models/project.js
@@ -1,12 +1,10 @@
 class Project {
-
-    constructor(id, name, description, times)
-    {
-        this.id=id;
-        this.name=name;
-        this.description=description;
-        this.times=times;
-    }
+  constructor(id, name, description, times) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.times = times;
+  }
 }
 
 module.exports = Project;
--- server/models/time.js
+++ server/models/time.js
@@ -1,10 +1,8 @@
 class Time {
-
-    constructor(id, description, hours)
-    {
-        this.id=id;
-        this.description=description;
-        this.hours=hours;
-    }
+  constructor(id, description, hours) {
+    this.id = id;
+    this.description = description;
+    this.hours = hours;
+  }
 }
-module.exports=Time;
+module.exports = Time;
